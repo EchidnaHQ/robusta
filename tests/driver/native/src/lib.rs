3800a63065338c3a74ecdc00b8618d32da3dc10d
Nefo Fortressia <nefothingy@hotmail.com>
2022-07-30 17:12:50 +0700

    Make the test compatible with the new value-based API
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- tests/driver/native/src/lib.rs
+++ tests/driver/native/src/lib.rs
@@ -4,86 +4,150 @@ use robusta_jni::bridge;
 pub mod jni {
     use std::convert::TryInto;
 
-    use robusta_jni::convert::{IntoJavaValue, JValueWrapper, Signature, TryFromJavaValue, TryIntoJavaValue};
-    use robusta_jni::jni::JNIEnv;
-    use robusta_jni::jni::objects::AutoLocal;
+    use robusta_jni::convert::{
+        IntoJavaValue, JValueWrapper, Signature, TryFromJavaValue, TryIntoJavaValue,
+    };
     use robusta_jni::jni::errors::Result as JniResult;
+    use robusta_jni::jni::objects::AutoLocal;
+    use robusta_jni::jni::JNIEnv;
 
     #[derive(Signature, TryIntoJavaValue, IntoJavaValue, TryFromJavaValue)]
     #[package()]
-    pub struct User<'env: 'borrow, 'borrow> {
+    pub struct User<'env> {
         #[instance]
-        raw: AutoLocal<'env, 'borrow>,
-        password: String
+        raw: AutoLocal<'env>,
+        password: String,
     }
 
-    impl<'env: 'borrow, 'borrow> User<'env, 'borrow> {
+    impl<'env> User<'env> {
         pub extern "jni" fn initNative() {
             std::env::var("RUST_LOG").unwrap_or_else(|_| {
                 std::env::set_var("RUST_LOG", "info");
                 "info".to_string()
             });
-            println!("Initialized env logger with level: {}", std::env::var("RUST_LOG").unwrap());
+            println!(
+                "Initialized env logger with level: {}",
+                std::env::var("RUST_LOG").unwrap()
+            );
             env_logger::init();
         }
 
-        pub extern "jni" fn userCountStatus(env: &JNIEnv) -> String {
-            let users_count: i32 = JValueWrapper::from(env.get_static_field("User", "TOTAL_USERS_COUNT", "I").unwrap()).try_into().unwrap();
+        pub extern "jni" fn userCountStatus(env: JNIEnv) -> String {
+            let users_count: i32 = JValueWrapper::from(
+                env.get_static_field("User", "TOTAL_USERS_COUNT", "I")
+                    .unwrap(),
+            )
+            .try_into()
+            .unwrap();
             users_count.to_string()
         }
 
-        pub extern "jni" fn hashedPassword(self, _env: &JNIEnv, _seed: i32) -> String {
+        pub extern "jni" fn hashedPassword(self, _env: JNIEnv, _seed: i32) -> String {
             let user_pw: String = self.password;
             user_pw + "_pass"
         }
 
-        pub extern "jni" fn getInt(self, v: i32) -> i32 { v }
+        pub extern "jni" fn getInt(self, v: i32) -> i32 {
+            v
+        }
 
-        pub extern "jni" fn getBool(self, v: bool) -> bool { v }
+        pub extern "jni" fn getBool(self, v: bool) -> bool {
+            v
+        }
 
-        pub extern "jni" fn getChar(self, v: char) -> char { v }
+        pub extern "jni" fn getChar(self, v: char) -> char {
+            v
+        }
 
-        pub extern "jni" fn getByte(self, v: i8) -> i8 { v }
+        pub extern "jni" fn getByte(self, v: i8) -> i8 {
+            v
+        }
 
-        pub extern "jni" fn getFloat(self, v: f32) -> f32 { v }
+        pub extern "jni" fn getFloat(self, v: f32) -> f32 {
+            v
+        }
 
-        pub extern "jni" fn getDouble(self, v: f64) -> f64 { v }
+        pub extern "jni" fn getDouble(self, v: f64) -> f64 {
+            v
+        }
 
-        pub extern "jni" fn getLong(self, v: i64) -> i64 { v }
+        pub extern "jni" fn getLong(self, v: i64) -> i64 {
+            v
+        }
 
-        pub extern "jni" fn getShort(self, v: i16) -> i16 { v }
+        pub extern "jni" fn getShort(self, v: i16) -> i16 {
+            v
+        }
 
-        pub extern "jni" fn getString(self, v: String) -> String { v }
+        pub extern "jni" fn getString(self, v: String) -> String {
+            v
+        }
 
-        pub extern "jni" fn getIntArray(self, v: Vec<i32>) -> Vec<i32> { v }
+        pub extern "jni" fn getIntArray(self, v: Vec<i32>) -> Vec<i32> {
+            v
+        }
 
-        pub extern "jni" fn getStringArray(self, v: Vec<String>) -> Vec<String> { v }
+        pub extern "jni" fn getStringArray(self, v: Vec<String>) -> Vec<String> {
+            v
+        }
 
-        pub extern "jni" fn intToString(self, v: i32) -> String { format!("{}", v) }
+        pub extern "jni" fn intToString(self, v: i32) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn boolToString(self, v: bool) -> String { format!("{}", v) }
+        pub extern "jni" fn boolToString(self, v: bool) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn charToString(self, v: char) -> String { format!("{}", v) }
+        pub extern "jni" fn charToString(self, v: char) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn byteToString(self, v: i8) -> String { format!("{}", v) }
+        pub extern "jni" fn byteToString(self, v: i8) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn floatToString(self, v: f32) -> String { format!("{}", v) }
+        pub extern "jni" fn floatToString(self, v: f32) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn doubleToString(self, v: f64) -> String { format!("{}", v) }
+        pub extern "jni" fn doubleToString(self, v: f64) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn longToString(self, v: i64) -> String { format!("{}", v) }
+        pub extern "jni" fn longToString(self, v: i64) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn shortToString(self, v: i16) -> String { format!("{}", v) }
+        pub extern "jni" fn shortToString(self, v: i16) -> String {
+            format!("{}", v)
+        }
 
-        pub extern "jni" fn intArrayToString(self, v: Vec<i32>) -> String { format!("{:?}", v) }
+        pub extern "jni" fn intArrayToString(self, v: Vec<i32>) -> String {
+            format!("{:?}", v)
+        }
 
-        pub extern "jni" fn stringArrayToString(self, v: Vec<String>) -> String { format!("{:?}", v) }
+        pub extern "jni" fn stringArrayToString(self, v: Vec<String>) -> String {
+            format!("{:?}", v)
+        }
 
-        pub extern "java" fn getPassword(&self, env: &JNIEnv) -> ::robusta_jni::jni::errors::Result<String> {}
+        pub extern "java" fn getPassword(
+            &self,
+            env: JNIEnv,
+        ) -> ::robusta_jni::jni::errors::Result<String> {
+        }
 
-        pub extern "java" fn getTotalUsersCount(env: &JNIEnv) -> ::robusta_jni::jni::errors::Result<i32> {}
+        pub extern "java" fn getTotalUsersCount(
+            env: JNIEnv,
+        ) -> ::robusta_jni::jni::errors::Result<i32> {
+        }
 
         #[constructor]
-        pub extern "java" fn new(env: &'borrow JNIEnv<'env>, username: String, password: String) -> JniResult<Self> {}
+        pub extern "java" fn new(
+            env: JNIEnv<'env>,
+            username: String,
+            password: String,
+        ) -> JniResult<Self> {
+        }
     }
 }
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,10 +1,10 @@
-use std::process::Command;
-use robusta_jni::jni::{InitArgsBuilder, JavaVM, JNIEnv};
-use native::jni::User;
 use jni::objects::JString;
+use native::jni::User;
 use robusta_jni::convert::FromJavaValue;
+use robusta_jni::jni::{InitArgsBuilder, JNIEnv, JavaVM};
+use std::process::Command;
 
-fn print_exception(env: &JNIEnv) -> jni::errors::Result<()> {
+fn print_exception(env: JNIEnv) -> jni::errors::Result<()> {
     let ex = env.exception_occurred()?;
     env.exception_clear()?;
     let res = env.call_method(ex, "toString", "()Ljava/lang/String;", &[])?;
@@ -13,7 +13,6 @@ fn print_exception(env: &JNIEnv) -> jni::errors::Result<()> {
     println!("Java exception occurred: {}", s);
     Ok(())
 }
-
 
 #[test]
 fn java_integration_tests() {
@@ -42,32 +41,39 @@ fn vm_creation_and_object_usage() {
     let current_dir = std::env::current_dir().expect("Couldn't get current dir");
     let classpath = current_dir.join("./tests/driver/build/classes/java/main");
 
-    let vm_args  = InitArgsBuilder::new()
-        .option(&*format!("-Djava.class.path={}", classpath.to_string_lossy()))
-        .build().expect("can't create vm args");
+    let vm_args = InitArgsBuilder::new()
+        .option(&*format!(
+            "-Djava.class.path={}",
+            classpath.to_string_lossy()
+        ))
+        .build()
+        .expect("can't create vm args");
     let vm = JavaVM::new(vm_args).expect("can't create vm");
     let env = vm.attach_current_thread().expect("can't get vm env");
 
     User::initNative();
 
-    let count = User::getTotalUsersCount(&env)
-            .or_else(|e| {
-                let _ = print_exception(&env);
-                Err(e)
-            })
-            .expect("can't get user count");
+    let count = User::getTotalUsersCount(*env)
+        .or_else(|e| {
+            let _ = print_exception(*env);
+            Err(e)
+        })
+        .expect("can't get user count");
 
     assert_eq!(count, 0);
 
-    let u = User::new(&env, "user".into(), "password".into()).expect("can't create user instance");
+    let u = User::new(*env, "user".into(), "password".into()).expect("can't create user instance");
 
-    let count = User::getTotalUsersCount(&env)
+    let count = User::getTotalUsersCount(*env)
         .or_else(|e| {
-            let _ = print_exception(&env);
+            let _ = print_exception(*env);
             Err(e)
         })
         .expect("can't get user count");
     assert_eq!(count, 1);
 
-    assert_eq!(u.getPassword(&env).expect("can't get user password"), "password")
+    assert_eq!(
+        u.getPassword(*env).expect("can't get user password"),
+        "password"
+    )
 }
